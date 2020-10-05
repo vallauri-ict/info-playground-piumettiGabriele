fdb35cf175bd54791aa41efe9e9fe8f8ff067343
piumettiGabriele <71816557+piumettiGabriele@users.noreply.github.com>
2020-10-05 09:47:31 +0200

    Update query.sql

--- EsSQL_Musei/QUERY/query.sql
+++ EsSQL_Musei/QUERY/query.sql
@@ -11,4 +11,16 @@ SELECT * FROM OPERE o, ARTISTI a WHERE a.nazionalita = 'Italiana' AND o.nomeA = 
 SELECT * FROM OPERE o, ARTISTI a WHERE a.nomeA = o.nomeA AND a.nazionalita = 'Italiana' GROUP BY o.nomeM HAVING COUNT(*)>=2;
 
 /* 11 */
-SELECT 
+SELECT o.NomeA, count(*) AS NumeroOpere FROM Opere o
+WHERE o.NomeM = 'Fenice' 
+GROUP BY o.NomeA;
+
+/* 10 */
+SELECT * FROM Musei m
+WHERE m.Citta = 'Ancona'
+AND NOT EXISTS (SELECT * FROM Opere o WHERE o.NomeM = m.NomeM AND o.NomeA <> 'Barbero');
+
+/* 10bis */
+SELECT m.nomeM FROM Musei m
+WHERE m.Citta = 'Ancona'
+AND 'Barbero' = ALL(SELECT o.nomeA FROM OPERE o WHERE o.nomeM = m.nomeM);
